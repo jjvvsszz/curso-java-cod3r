e0bd9a703fa436d8c5d1436c19567c8640be32c0
jjvvsszz <jjvvsszz.gamer@gmail.com>
2021-12-22 19:46:02 -0300

    Desafio aula 105

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -4,12 +4,11 @@ public class ProdutoTeste {
 
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+        Produto p1 = new Produto("Notebook", 4356.89);
 
         var p2 = new Produto();
         p2.nome = "Caneta Preta";
         p2.preco = 12.56;
-        p2.desconto = 0.29;
 
         System.out.println(p1.nome);
         System.out.println(p1.precoFinal(50.0));
