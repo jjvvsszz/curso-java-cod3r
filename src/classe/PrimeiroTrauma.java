8aad22f85d55e8e67e98cad5ab67a7ebf3e39c59
jjvvsszz <jjvvsszz.gamer@gmail.com>
2021-12-23 14:46:13 -0300

    Resposta desafio aula 110

--- src/classe/PrimeiroTrauma.java
+++ src/classe/PrimeiroTrauma.java
@@ -3,10 +3,12 @@ package classe;
 public class PrimeiroTrauma {
 
     int a = 3;
+    static int b = 4;
 
     public static void main(String[] args) {
 
         PrimeiroTrauma a = new PrimeiroTrauma();
         System.out.println(a.a);
+        System.out.println(b);
     }
 }
